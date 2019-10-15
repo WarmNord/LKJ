4eadd882194d3e4a6aa13814a73fa6a84489851b
Ekaterina Funtikova <funtikova@siams.com>
2019-10-15 11:19:44 +0500

    4

---
+++ src/jcourse/hw/dffd.java
@@ -0,0 +1,19 @@
+package jcourse.hw;
+
+import java.util.Scanner;
+
+public class dffd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if ((-3 <= a || a <= 1) || (5 <= a || a <= 9));{
+
+            System.out.println("YES");
+        }
+
+
+
+    }
+    }
