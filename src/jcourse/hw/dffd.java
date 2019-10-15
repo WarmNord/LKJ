ee0426100a4a4d23d6dfcf5c24f516d8a03777f0
Ekaterina Funtikova <funtikova@siams.com>
2019-10-15 17:53:49 +0500

    5

--- src/jcourse/hw/dffd.java
+++ src/jcourse/hw/dffd.java
@@ -6,13 +6,6 @@ public class dffd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-
-        if ((-3 <= a || a <= 1) || (5 <= a || a <= 9));{
-
-            System.out.println("YES");
-        }
-
 
 
     }
