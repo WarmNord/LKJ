b76c4b2788a8592d479cb2c9def8cff11aee6a5a
Ekaterina Funtikova <funtikova@siams.com>
2019-10-22 17:54:16 +0500

    5

---
+++ src/jcourse/hw/HW07.java
@@ -0,0 +1,9 @@
+package jcourse.hw;
+
+public class HW07 {
+    public static void main(String[] args) {
+
+        int[] first = new int[900];
+
+    }
+}
---
+++ src/jcourse/training/Arrays.java
@@ -0,0 +1,13 @@
+package jcourse.training;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int [] [] table = new int[2][2];
+
+        table [0] [0] = 1;
+        table [0] [1] = 2;
+
+
+    }
+}
