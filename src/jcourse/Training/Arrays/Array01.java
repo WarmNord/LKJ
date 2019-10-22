505abb1f810c3fe2c7c7d99f148b3c1805df0a93
funtikova <funtikova95@mail.ru>
2019-10-22 22:28:47 +0500

    arrays

---
+++ src/jcourse/Training/Arrays/Array01.java
@@ -0,0 +1,20 @@
+package jcourse.Training.Arrays;
+
+public class Array01 {
+
+    public static void main(String[] args) {
+
+    int[] ar = new int[10];
+
+
+    for (int a = 0; a < 10; a++) {
+
+        ar[a] = a + 2;
+
+
+        System.out.println(ar[a]);
+    }
+    }
+
+
+}
--- src/jcourse/hw/HW07.java
+++ src/jcourse/hw/HW07.java
@@ -5,5 +5,23 @@ public class HW07 {
 
         int[] first = new int[900];
 
+        for (int i = 0; i < first.length; i++) {
+
+            first[i] = i + 100;
+
+        }
+
+        int[] second = new int[first.length];
+
+        for (int i = 0, j = first.length - 1; i < first.length; i++, j--) {
+
+            second[j] = first[i];
+
+        }
+
+        for (int i : second) {
+            System.out.println(i);
+        }
+
     }
 }
