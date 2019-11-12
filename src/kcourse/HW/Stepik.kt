c47b96e04d241bef4026b386215f744a8fc75c77
Ekaterina Funtikova <funtikova@siams.com>
2019-11-12 17:54:55 +0500

    new 2

---
+++ src/kcourse/HW/Stepik.kt
@@ -0,0 +1,16 @@
+package kcourse.HW
+
+val scan = java.util.Scanner(System.`in`)
+
+fun main() {
+
+    val a = scan.nextInt()
+    val b = scan.nextInt()
+
+    if (a > b ) {
+        println("true")
+    } else {
+        println("false")
+    }
+
+}
