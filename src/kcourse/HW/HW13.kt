5f80eb5ee73e530a408f12e554977fd381bba302
Ekaterina Funtikova <funtikova95@mail.ru>
2020-03-11 18:05:15 +0500

    L13

--- src/kcourse/HW/HW13.kt
+++ src/kcourse/HW/HW13.kt
@@ -2,12 +2,12 @@ package kcourse.HW
 
 fun main() {
     val initArray = mutableListOf<Int>()
-    for (i in 0 until 1000){
-        initArray.add((Math.random() * 1000).toInt() )
+
+    for (i in 0 until 1000) {
+        initArray.add((Math.random() * 1000).toInt())
     }
 
-    val result = initArray.filter { it % 5 == 0 || it % 3 ==0 }.map { it * it }.sortedDescending().map { "$it" }
-    for (i in result) {
-        println(i)
-    }
-}
+    val a = initArray.filter { it % 5 == 0 || it % 3 == 0 }.map { it * it }.sortedDescending().map { "Wow $it" }
+
+}}
+
--- src/kcourse/Section3/L13.kt
+++ src/kcourse/Section3/L13.kt
@@ -2,18 +2,54 @@ package kcourse.Section3
 
 fun main() {
 
-    val listOfNumbers = mutableListOf<Int>()
+   /* val listOfNumbers = mutableListOf<Int>()
+        for (i in 0..99) {
+            listOfNumbers.add(i)
+        }
 
-    for (i in 0..99){
-       listOfNumbers.add(i)
+    val listOfEvenNumbers = listOfNumbers.filter { it % 2 == 0 }
+
+     for (i in listOfEvenNumbers)
+     {
+         println(i)
+     }
+
+
+    val listNames = mutableListOf<String>()
+
+    for (i in 0..10) {
+        listNames.add("A$i")
+        listNames.add("B$i")
     }
 
-    val listOfEvenNumbers = listOfNumbers.filter ({ number: Int -> number % 2 == 0})
+    val listNamesA = listNames.filter { it.startsWith("B")}
 
-    for (i in listOfEvenNumbers){
+    for (i in listNamesA)
+    {
+        println(i)
+    }*/
+
+    val numbers = (0..100).toList() // list of numbers from 0 to 100
+
+  /*  val doubledNumbers = numbers.map { it * 2 } // Method MAP Element in new COllections may be different type
+
+    for (i in doubledNumbers)
+    {
+        println(i)
+    }*/
+
+   /* val employees = numbers.map { "Employee #$it" }
+
+    for ( i in employees)
+    {
+        println(i)
+    }*/
+
+    val array = arrayOf(6,4,56,-9,67)
+    val sArray = array.sortedDescending()
+
+    for (i in sArray){
         println(i)
     }
-
-
 
 }
